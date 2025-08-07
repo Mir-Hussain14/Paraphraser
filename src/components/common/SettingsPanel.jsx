f38dcefae2f6c031f5c93c56a3e8e995e8fe10fd
Tanzeel Ur Rehman <khawajatanzeel.55221@gmail.com>
2025-08-08 03:04:27 +0500

    Profile page completed

--- src/components/common/SettingsPanel.jsx
+++ src/components/common/SettingsPanel.jsx
@@ -1,4 +1,5 @@
-import { Settings, Moon, LogOut, ChevronRight, X } from "lucide-react"
+import { Settings, Moon } from "lucide-react";
+import { Link } from "react-router-dom";
 
 export default function SettingsPanel({ darkMode, setDarkMode, onClose }) {
   return (
@@ -7,12 +8,19 @@ export default function SettingsPanel({ darkMode, setDarkMode, onClose }) {
         darkMode ? "bg-[#101214] border-gray-900" : "bg-white border-gray-200"
       }`}
     >
-
       {/* Dark Mode Toggle */}
       <div className="flex items-center justify-between">
         <div className="flex items-center space-x-2">
-          <Moon className={`w-4 h-4 transition-colors duration-300 ${darkMode ? "text-gray-300" : "text-gray-600"}`} />
-          <span className={`transition-colors duration-300 ${darkMode ? "text-gray-200" : "text-gray-700"}`}>
+          <Moon
+            className={`w-4 h-4 transition-colors duration-300 ${
+              darkMode ? "text-gray-300" : "text-gray-600"
+            }`}
+          />
+          <span
+            className={`transition-colors duration-300 ${
+              darkMode ? "text-gray-200" : "text-gray-700"
+            }`}
+          >
             Dark Mode
           </span>
         </div>
@@ -29,6 +37,27 @@ export default function SettingsPanel({ darkMode, setDarkMode, onClose }) {
           />
         </button>
       </div>
+
+      {/* Settings Option Link */}
+      <div className="mt-6">
+        <Link
+          to="/settings"
+          className="flex items-center space-x-2 text-sm font-medium transition-colors duration-300 hover:text-lime-400"
+        >
+          <Settings
+            className={`w-5 h-5 ${
+              darkMode ? "text-gray-300" : "text-gray-600"
+            }`}
+          />
+          <span
+            className={`transition-colors duration-300 ${
+              darkMode ? "text-gray-200" : "text-gray-700"
+            }`}
+          >
+            Settings
+          </span>
+        </Link>
+      </div>
     </div>
-  )
+  );
 }
---
+++ src/components/settings/Password.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export const Password = () => {
+  return <div className="md:px-[30px] py-7">Password</div>;
+}
